93710766ce466e7dc1ae18bbae5f09f46d2cf08c
Y-Jimin <jak2899@naver.com>
2026-03-24 20:39:39 +0900

    fix:시작 공통부분 수정

--- src/test/java/com/aikids/care/DemoApplicationTests.java
+++ src/test/java/com/aikids/care/DemoApplicationTests.java
@@ -3,7 +3,9 @@ package com.aikids.care;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	"spring.autoconfigure.exclude=org.springframework.ai.model.google.genai.autoconfigure.chat.GoogleGenAiChatAutoConfiguration"
+})
 class DemoApplicationTests {
 
 	@Test
