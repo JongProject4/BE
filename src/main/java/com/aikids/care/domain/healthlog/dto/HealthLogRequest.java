805bf4b50266861881d16c580600147a340cb3dc
Hakfe <cjf1685@gmail.com>
2026-04-02 20:31:53 +0900

    Feat: HealthLog DTO 생성 (#3)

---
+++ src/main/java/com/aikids/care/domain/healthlog/dto/HealthLogRequest.java
@@ -0,0 +1,23 @@
+package com.aikids.care.domain.healthlog.dto;
+
+import com.aikids.care.domain.healthlog.HealthLog.LogType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class HealthLogRequest {
+
+    @NotNull(message = "로그 유형은 필수입니다.")
+    private LogType logType;
+
+    @NotBlank(message = "내용은 필수입니다.")
+    private String content;
+
+    @NotNull(message = "사건 발생 일시는 필수입니다.")
+    private LocalDateTime eventDate;
+}
