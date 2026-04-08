81de4ce650211dedb0a5e99eff4bba3ee87cecfa
Y-Jimin <jak2899@naver.com>
2026-04-08 17:21:02 +0900

    feat: google oauth 구현을 위한 SecurityConfig 옵션 추가

--- src/main/java/com/aikids/care/global/security/SecurityConfig.java
+++ src/main/java/com/aikids/care/global/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.aikids.care.global.security;
 
 import com.aikids.care.domain.user.service.CustomOAuth2UserService;
+import com.aikids.care.global.security.oauth2.OAuth2AuthenticationSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 	private final CustomOAuth2UserService customOAuth2UserService;
+	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,6 +24,7 @@ public class SecurityConfig {
 						.anyRequest().permitAll()
 				)
 				.oauth2Login(oauth2 -> oauth2
+						.successHandler(oAuth2AuthenticationSuccessHandler)
 						.userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
 				);
 
