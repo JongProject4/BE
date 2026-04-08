3a0c98373d31601acc135e2c8657fcd8a61b5c5c
Y-Jimin <jak2899@naver.com>
2026-04-08 17:30:19 +0900

    feat: jwt 및 oauth2 설정 추가

--- src/main/java/com/aikids/care/DemoApplication.java
+++ src/main/java/com/aikids/care/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.aikids.care;
 
+import com.aikids.care.global.security.jwt.JwtProperties;
+import com.aikids.care.global.security.oauth2.OAuth2FrontendProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ JwtProperties.class, OAuth2FrontendProperties.class })
 public class DemoApplication {
 
 	public static void main(String[] args) {
